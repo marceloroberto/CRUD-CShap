8c328328d54fd2adfea6ae41c3cf4aebbf04bbc6
Marcelo Roberto <marcelo@marceloroberto.com.br>
2021-05-29 15:12:51 -0300

    Primeiro commit do projeto

--- CRUDEF_TopicosdeTI/Form1.Designer.cs
+++ CRUDEF_TopicosdeTI/Form1.Designer.cs
@@ -29,20 +29,238 @@ namespace CRUDEF_TopicosdeTI
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.tb_sobrenome = new System.Windows.Forms.TextBox();
+            this.tb_cidade = new System.Windows.Forms.TextBox();
+            this.tb_endereco = new System.Windows.Forms.TextBox();
+            this.b_salvar = new System.Windows.Forms.Button();
+            this.b_excluir = new System.Windows.Forms.Button();
+            this.b_cancelar = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sobrenome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.endereco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(34, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(34, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(34, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Endereço";
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tb_nome.Location = new System.Drawing.Point(153, 29);
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.Size = new System.Drawing.Size(185, 20);
+            this.tb_nome.TabIndex = 4;
+            // 
+            // tb_sobrenome
+            // 
+            this.tb_sobrenome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tb_sobrenome.Location = new System.Drawing.Point(153, 76);
+            this.tb_sobrenome.Name = "tb_sobrenome";
+            this.tb_sobrenome.Size = new System.Drawing.Size(185, 20);
+            this.tb_sobrenome.TabIndex = 5;
+            // 
+            // tb_cidade
+            // 
+            this.tb_cidade.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tb_cidade.Location = new System.Drawing.Point(153, 126);
+            this.tb_cidade.Name = "tb_cidade";
+            this.tb_cidade.Size = new System.Drawing.Size(185, 20);
+            this.tb_cidade.TabIndex = 6;
+            // 
+            // tb_endereco
+            // 
+            this.tb_endereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tb_endereco.Location = new System.Drawing.Point(153, 174);
+            this.tb_endereco.Multiline = true;
+            this.tb_endereco.Name = "tb_endereco";
+            this.tb_endereco.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.tb_endereco.Size = new System.Drawing.Size(185, 74);
+            this.tb_endereco.TabIndex = 7;
+            // 
+            // b_salvar
+            // 
+            this.b_salvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_salvar.ForeColor = System.Drawing.Color.Cornsilk;
+            this.b_salvar.Location = new System.Drawing.Point(31, 274);
+            this.b_salvar.Name = "b_salvar";
+            this.b_salvar.Size = new System.Drawing.Size(96, 36);
+            this.b_salvar.TabIndex = 8;
+            this.b_salvar.Text = "Salvar";
+            this.b_salvar.UseVisualStyleBackColor = true;
+            this.b_salvar.Click += new System.EventHandler(this.b_salvar_Click);
+            // 
+            // b_excluir
+            // 
+            this.b_excluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_excluir.ForeColor = System.Drawing.Color.Cornsilk;
+            this.b_excluir.Location = new System.Drawing.Point(145, 274);
+            this.b_excluir.Name = "b_excluir";
+            this.b_excluir.Size = new System.Drawing.Size(96, 36);
+            this.b_excluir.TabIndex = 9;
+            this.b_excluir.Text = "Excluir";
+            this.b_excluir.UseVisualStyleBackColor = true;
+            // 
+            // b_cancelar
+            // 
+            this.b_cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b_cancelar.ForeColor = System.Drawing.Color.Cornsilk;
+            this.b_cancelar.Location = new System.Drawing.Point(259, 274);
+            this.b_cancelar.Name = "b_cancelar";
+            this.b_cancelar.Size = new System.Drawing.Size(96, 36);
+            this.b_cancelar.TabIndex = 10;
+            this.b_cancelar.Text = "Cancelar";
+            this.b_cancelar.UseVisualStyleBackColor = true;
+            this.b_cancelar.Click += new System.EventHandler(this.b_cancelar_Click);
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.codigo,
+            this.nome,
+            this.sobrenome,
+            this.cidade,
+            this.endereco});
+            this.grid.Location = new System.Drawing.Point(378, 29);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.Size = new System.Drawing.Size(410, 281);
+            this.grid.TabIndex = 11;
+            // 
+            // codigo
+            // 
+            this.codigo.DataPropertyName = "cli_codigo";
+            this.codigo.HeaderText = "Código";
+            this.codigo.Name = "codigo";
+            this.codigo.ReadOnly = true;
+            this.codigo.Visible = false;
+            // 
+            // nome
+            // 
+            this.nome.DataPropertyName = "cli_nome";
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            this.nome.ReadOnly = true;
+            // 
+            // sobrenome
+            // 
+            this.sobrenome.DataPropertyName = "cli_sobrenome";
+            this.sobrenome.HeaderText = "Sobrenome";
+            this.sobrenome.Name = "sobrenome";
+            this.sobrenome.ReadOnly = true;
+            // 
+            // cidade
+            // 
+            this.cidade.DataPropertyName = "cli_cidade";
+            this.cidade.HeaderText = "Cidade";
+            this.cidade.Name = "cidade";
+            this.cidade.ReadOnly = true;
+            // 
+            // endereco
+            // 
+            this.endereco.DataPropertyName = "cli_endereco";
+            this.endereco.HeaderText = "Endereço";
+            this.endereco.Name = "endereco";
+            this.endereco.ReadOnly = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 250);
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.ClientSize = new System.Drawing.Size(812, 333);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.b_cancelar);
+            this.Controls.Add(this.b_excluir);
+            this.Controls.Add(this.b_salvar);
+            this.Controls.Add(this.tb_endereco);
+            this.Controls.Add(this.tb_cidade);
+            this.Controls.Add(this.tb_sobrenome);
+            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "CRUD - Entity Framework";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.TextBox tb_sobrenome;
+        private System.Windows.Forms.TextBox tb_cidade;
+        private System.Windows.Forms.TextBox tb_endereco;
+        private System.Windows.Forms.Button b_salvar;
+        private System.Windows.Forms.Button b_excluir;
+        private System.Windows.Forms.Button b_cancelar;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sobrenome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endereco;
     }
 }
 
--- CRUDEF_TopicosdeTI/Form1.cs
+++ CRUDEF_TopicosdeTI/Form1.cs
@@ -7,14 +7,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity; // Declarando a variável do Entity Framework
 
 namespace CRUDEF_TopicosdeTI
 {
     public partial class Form1 : Form
     {
+
+        void limpar()
+        {
+            tb_nome.Clear();
+            tb_sobrenome.Clear();
+            tb_cidade.Clear();
+            tb_endereco.Clear();
+            tb_nome.Focus();
+            b_salvar.Text = "Salvar";
+            cli.cli_codigo = 0;
+        }
+
+        void preenchegrid()
+        {
+            grid.AutoGenerateColumns = false;
+            using (DBEntidade db = new DBEntidade())
+            {
+                grid.DataSource = db.Cliente.ToList<Cliente>();
+            }
+        }
+
+        Cliente cli = new Cliente(); // Faz a instância do banco
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void b_cancelar_Click(object sender, EventArgs e)
+        {
+            limpar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            limpar();
+            preenchegrid();
+        }
+
+        private void b_salvar_Click(object sender, EventArgs e)
+        {
+            cli.cli_nome = tb_nome.Text.Trim();
+            cli.cli_sobrenome = tb_sobrenome.Text.Trim();
+            cli.cli_cidade = tb_cidade.Text.Trim();
+            cli.cli_endereco = tb_endereco.Text.Trim();
+
+            DBEntidade db = new DBEntidade();
+
+            if (cli.cli_codigo == 0)
+                db.Cliente.Add(cli);
+            else            
+                db.Entry(cli).State = EntityState.Modified; // Colocar a biblioteca using System.Data.Entity;
+            db.SaveChanges();
+
+            limpar();
+            preenchegrid();
+
+            MessageBox.Show("Dados gravados com sucesso");
+
         }
     }
 }
